294b56ce785c7e5a1638bec2c47545b2a4e5dccd
SolidCode109 <krzysztof.siczek109@gmail.com>
2025-03-25 14:20:06 +0100

    Update ThemeSwitch.js

--- src/components/ThemeSwitch/ThemeSwitch.js
+++ src/components/ThemeSwitch/ThemeSwitch.js
@@ -12,25 +12,25 @@ export function ThemeSwitch() {
   
     useEffect(() =>  setMounted(true), [])
   
-    if (!mounted) return (
-      <Image
-        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={36}
-        height={36}
-        sizes="36x36"
-        alt="Loading Light/Dark Toggle"
-        priority={false}
-        title="Loading Light/Dark Toggle"
-      />
+    return (
+      <>
+        {!mounted ? (
+          <Image
+            src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+            width={32}
+            height={32}
+            sizes="32x32"
+            alt="Loading Light/Dark Toggle"
+            priority={false}
+            title="Loading Light/Dark Toggle"
+          />
+        ) : resolvedTheme === 'dark' ? (
+          <FiSun onClick={() => setTheme('light')} />
+        ) : (
+          <FiMoon onClick={() => setTheme('dark')} />
+        )}
+      </>
     )
-  
-    if (resolvedTheme === 'dark') {
-      return <FiSun onClick={() => setTheme('light')} />
-    }
-  
-    if (resolvedTheme === 'light') {
-      return <FiMoon onClick={() => setTheme('dark')} />
-    }
   
   }
 
